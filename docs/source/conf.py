350677afce0b9191cf72f125f011396fb6104849
thanachart29 <thanachart29@gmail.com>
2022-12-12 22:49:14 +0700

    add people perception docs ver1 add tab

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,7 @@ copyright = f"2022-{datetime.date.today().year}, {author}"
 
 # extensions = ['breathe', 'sphinx.ext.graphviz', 'sphinxcontrib.plantuml', 'sphinx.ext.extlinks']
 extensions = ['sphinx_tabs.tabs']
+sphinx_tabs_valid_builders = ['linkcheck']
 
 master_doc = "index"
 language = 'en'
